de635508de5bcf722475317dd2e868f20fb3c703
Paul Milovanov <paul@dada.science>
2021-03-29 14:10:20 -0400

    checkpoint

--- weather.py
+++ weather.py
@@ -1,7 +1,10 @@
 import csv
+import datetime
+import typing
 from typing import Type
 
 import apache_beam as beam
+from apache_beam.transforms.window import TimestampedValue
 
 OUT_PARSED = "parsed"
 OUT_INVALID = "invalid"
@@ -20,10 +23,29 @@ def parse_csv_kv_row(keytype: type, valtype: type, keylen: int = -1, vallen: int
     return fn
 
 
+class WeatherEntry(typing.NamedTuple):
+    stn: int
+    obsdate: datetime.datetime
+    temp: float
+    windspeed: float
+    tornado_or_funnel: bool
+
 def parse_weather_entry(line: str):
     for row in csv.reader([line]):
         assert len(row) == 16
-
+        stn, yearmoda, temp, windspeed, frshtt = row[0], row[2], row[3], row[8], row[15]
+        assert len(frshtt) == 6
+        if temp == "9999.9":
+            temp = "-1.0"
+        if windspeed == "999.9":
+            windspeed = "-1.0"
+        return WeatherEntry(
+            int(stn),
+            datetime.datetime.strptime(yearmoda, "%Y%m%d"),
+            float(temp),
+            float(windspeed),
+            frshtt[5] == "1",
+        )
 
 class ParseFn(beam.DoFn):
     def __init__(self, parser):
@@ -34,3 +56,8 @@ class ParseFn(beam.DoFn):
             yield beam.pvalue.TaggedOutput(OUT_PARSED, self.parser(element))
         except:
             yield beam.pvalue.TaggedOutput(OUT_INVALID, element)
+
+class EntryAddTimestampFn(beam.DoFn):
+    def process(self, element: WeatherEntry, *args, **kwargs):
+        yield TimestampedValue(element, int(element.obsdate.timestamp()))
+
--- weather_test.py
+++ weather_test.py
@@ -1,6 +1,9 @@
+import datetime
+
 import pytest
 
 from weather import *
+
 
 def test_parse_csv_kv_row():
     assert parse_csv_kv_row(int, str)("421110,IN") == (421110, "IN")
@@ -20,6 +23,7 @@ def test_parse_csv_kv_row():
     with pytest.raises(ValueError):
         assert parse_csv_kv_row(int, str)("HI,THERE")
 
+
 def test_parse_fn():
     pp = ParseFn(parse_csv_kv_row(int, str, 6, 2))
 
@@ -32,3 +36,18 @@ def test_parse_fn():
     for el in pp.process("121121,IND"):
         assert el.tag == "invalid"
         assert el.value == "121121,IND"
+
+
+def test_parse_weather_entry():
+    assert parse_weather_entry(
+        "958360,99999,20190129,79.1,60.2,9999.9,9999.9,999.9,5.5,11.1,999.9,93.6*,65.3,0.11G,999.9,010000"
+    ) == WeatherEntry(958360, datetime.datetime(2019, 1, 29, 0, 0), 79.1, 5.5, False)
+
+    assert parse_weather_entry(
+        "958360,99999,20190129,9999.9,60.2,9999.9,9999.9,999.9,999.9,11.1,999.9,93.6*,65.3,0.11G,999.9,010001"
+    ) == WeatherEntry(958360, datetime.datetime(2019, 1, 29, 0, 0), -1.0, -1.0, True)
+
+
+# Which country had the hottest average mean temperature over the year?
+# Which country had the most consecutive days of tornadoes/funnel cloud formations?
+# Which country had the second highest average mean wind speed over the year?
